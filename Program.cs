dbf79f91241fb112b58164a838d2b7b75e50343a
Akshay Tembe <akshaytembe625@gmail.com>
2025-07-19 22:55:48 +0530

    Initial Commit for consoleApp5

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the size of an array");
+            uint[] inparr=new uint[5] {1,2,3,4,5};
+            int num = 5;
+            List<List<int>> retlist = calcfun( inparr, num);
+            foreach (var i in retlist) {
+                Console.WriteLine($"the first index is {i[0]} and second index is {i[1]}");
+            }
+        }
+        public static List<List<int>> calcfun(uint[] inparr,int num)
+        {
+            List<List<int>> list = new List<List<int>>();
+            for (int i = 0; i < inparr.Length; i++)
+            {
+                for (int j = 0; j < inparr.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (inparr[i] + inparr[j] == num)
+                        {
+                            List<int> locallist = new List<int>();
+                            locallist.Add(i);
+                            locallist.Add(j);
+                            list.Add(locallist);
+                        }
+
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
